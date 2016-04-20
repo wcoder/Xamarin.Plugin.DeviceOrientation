455c2c6c826253eec75c1382381dad92d68284e9
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2016-04-21 00:00:11 +0300

    Changed impl for iOS

--- src/DeviceOrientation/Plugin.DeviceOrientation.iOS/DeviceOrientationImplementation.cs
+++ src/DeviceOrientation/Plugin.DeviceOrientation.iOS/DeviceOrientationImplementation.cs
@@ -5,23 +5,64 @@ namespace Plugin.DeviceOrientation
 {
 	public class DeviceOrientationImplementation : BaseDeviceOrientationImplementation
 	{
-		public override DeviceOrientations CurrentOrientation
+		public override DeviceOrientations CurrentOrientation =>
+			Convert(UIApplication.SharedApplication.StatusBarOrientation);
+
+		//		private static DeviceOrientations CurrentDeviceOrientation
+		//		{
+		//			get
+		//			{
+		//				switch (UIDevice.CurrentDevice.Orientation)
+		//				{
+		//					case UIDeviceOrientation.Portrait:
+		//						return DeviceOrientations.Portrait;
+		//					case UIDeviceOrientation.PortraitUpsideDown:
+		//						return DeviceOrientations.PortraitFlipped;
+		//					case UIDeviceOrientation.LandscapeRight:
+		//						return DeviceOrientations.Landscape;
+		//					case UIDeviceOrientation.LandscapeLeft:
+		//						return DeviceOrientations.LandscapeFlipped;
+		//					default:
+		//						return DeviceOrientations.Undefined;
+		//				}
+		//			}
+		//		}
+
+		// https://docs.xamarin.com/api/property/UIKit.UIApplication.DidChangeStatusBarOrientationNotification/
+		public DeviceOrientationImplementation()
 		{
-			get
+			//var notificationCenter = NSNotificationCenter.DefaultCenter;
+			//notificationCenter.AddObserver(UIApplication.DidChangeStatusBarOrientationNotification, DeviceOrientationDidChange);
+			//UIDevice.CurrentDevice.BeginGeneratingDeviceOrientationNotifications();
+
+			UIApplication.Notifications.ObserveDidChangeStatusBarOrientation(DeviceOrientationDidChange);
+		}
+
+		/// <summary>
+		/// Devices the orientation did change.
+		/// </summary>
+		public void DeviceOrientationDidChange(object sender, UIStatusBarOrientationChangeEventArgs args)
+		{
+			OnOrientationChanged(new OrientationChangedEventArgs
 			{
-				switch (UIApplication.SharedApplication.StatusBarOrientation)
-				{
-					case UIInterfaceOrientation.Portrait:
-						return DeviceOrientations.Portrait;
-					case UIInterfaceOrientation.PortraitUpsideDown:
-						return DeviceOrientations.PortraitFlipped;
-					case UIInterfaceOrientation.LandscapeRight:
-						return DeviceOrientations.Landscape;
-					case UIInterfaceOrientation.LandscapeLeft:
-						return DeviceOrientations.LandscapeFlipped;
-					default:
-						return DeviceOrientations.Undefined;
-				}
+				Orientation = Convert(args.StatusBarOrientation)
+			});
+		}
+
+		private DeviceOrientations Convert(UIInterfaceOrientation orientation)
+		{
+			switch (orientation)
+			{
+				case UIInterfaceOrientation.Portrait:
+					return DeviceOrientations.Portrait;
+				case UIInterfaceOrientation.PortraitUpsideDown:
+					return DeviceOrientations.PortraitFlipped;
+				case UIInterfaceOrientation.LandscapeRight:
+					return DeviceOrientations.Landscape;
+				case UIInterfaceOrientation.LandscapeLeft:
+					return DeviceOrientations.LandscapeFlipped;
+				default:
+					return DeviceOrientations.Undefined;
 			}
 		}
 	}
