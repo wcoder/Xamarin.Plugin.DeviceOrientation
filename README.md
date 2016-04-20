decc752c266c5ef5d24ba5823b22cb5e5dc9c8b3
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2016-04-21 00:28:54 +0300

    Added impl for wp8.0

--- README.md
+++ README.md
@@ -8,11 +8,13 @@ Simple cross platform plugin to get screen orientation of mobile device.
 |Xamarin.iOS|+/-| |
 |Xamarin.iOS Unified|+/-| |
 |Xamarin.Android|Yes|API 3+|
-|Windows Phone Silverlight|-| |
+|Windows Phone Silverlight|+| |
 |Windows Phone RT|Yes|8.1+|
 |Windows Store RT|Yes|8.1+|
 |Windows 10 UWP|Yes|10+|
 |Xamarin.Mac|No||
+
+> +/- [not tested on device]
 
 #### Contributors
 * [Yauheni Pakala](https://github.com/wcoder)
--- src/DeviceOrientation/Plugin.DeviceOrientation.WindowsPhone8/DeviceOrientationImplementation.cs
+++ src/DeviceOrientation/Plugin.DeviceOrientation.WindowsPhone8/DeviceOrientationImplementation.cs
@@ -1,14 +1,73 @@
+using System.Windows;
+using Microsoft.Phone.Controls;
 using Plugin.DeviceOrientation.Abstractions;
+using OrientationChangedEventArgs = Microsoft.Phone.Controls.OrientationChangedEventArgs;
 
 namespace Plugin.DeviceOrientation
 {
-	public class DeviceOrientationImplementation : IDeviceOrientation
+	// https://msdn.microsoft.com/en-us/library/windows/apps/jj207002(v=vs.105).aspx#BKMK_Orientations
+	public class DeviceOrientationImplementation : BaseDeviceOrientationImplementation
 	{
-		public DeviceOrientations CurrentOrientation
+		private PhoneApplicationFrame _rootFrame;
+		private bool _disposed;
+
+		public override DeviceOrientations CurrentOrientation
 		{
 			get
 			{
-				return DeviceOrientations.Undefined;
+				if (_rootFrame == null)
+					return DeviceOrientations.Undefined;
+
+				return Convert(_rootFrame.Orientation);
+			}
+		}
+
+		public DeviceOrientationImplementation()
+		{
+			_rootFrame = Application.Current.RootVisual as PhoneApplicationFrame;
+			if (_rootFrame != null)
+			{
+				_rootFrame.OrientationChanged += RootFrameOnOrientationChanged;
+			}
+		}
+
+		public override void Dispose(bool disposing)
+		{
+			if (!_disposed)
+			{
+				if (disposing && _rootFrame != null)
+				{
+					_rootFrame.OrientationChanged -= RootFrameOnOrientationChanged;
+				}
+
+				_disposed = true;
+			}
+
+			base.Dispose(disposing);
+		}
+
+		private void RootFrameOnOrientationChanged(object sender, OrientationChangedEventArgs args)
+		{
+			OnOrientationChanged(new Abstractions.OrientationChangedEventArgs
+			{
+				Orientation = Convert(args.Orientation)
+			});
+		}
+
+		private DeviceOrientations Convert(PageOrientation orientation)
+		{
+			switch (orientation)
+			{
+				case PageOrientation.PortraitUp:
+					return DeviceOrientations.Portrait;
+				case PageOrientation.PortraitDown:
+					return DeviceOrientations.PortraitFlipped;
+				case PageOrientation.LandscapeLeft:
+					return DeviceOrientations.Landscape;
+				case PageOrientation.LandscapeRight:
+					return DeviceOrientations.LandscapeFlipped;
+				default:
+					return DeviceOrientations.Undefined;
 			}
 		}
 	}
