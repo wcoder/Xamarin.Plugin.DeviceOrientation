40beb611a7a82afa7097599ad74980890846152a
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2017-07-12 01:13:02 +0300

    Add xamarin.forms-sample

---
+++ samples/Xamarin.Forms-sample/TestNugetDeviceOrientationPlugin/TestNugetDeviceOrientationPlugin.Android/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Plugin.CurrentActivity;
+
+namespace TestNugetDeviceOrientationPlugin.Droid
+{
+    [Activity(Label = "TestNugetDeviceOrientationPlugin", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            CrossCurrentActivity.Current.Activity = this;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
---
+++ samples/Xamarin.Forms-sample/TestNugetDeviceOrientationPlugin/TestNugetDeviceOrientationPlugin/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms;
+using Plugin.DeviceOrientation;
+
+namespace TestNugetDeviceOrientationPlugin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            OrientationLabel.Text = CrossDeviceOrientation.Current.CurrentOrientation.ToString();
+            CrossDeviceOrientation.Current.OrientationChanged += Current_OrientationChanged;
+        }
+
+        private void Current_OrientationChanged(object sender, Plugin.DeviceOrientation.Abstractions.OrientationChangedEventArgs e)
+        {
+            OrientationLabel.Text = e.Orientation.ToString();
+        }
+    }
+}
