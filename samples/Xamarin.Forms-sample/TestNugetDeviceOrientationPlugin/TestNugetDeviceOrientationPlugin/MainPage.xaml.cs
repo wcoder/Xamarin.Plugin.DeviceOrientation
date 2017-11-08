d18a388e26e6c556b1e7b5dac4666c1e7839ec0f
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2017-11-08 21:25:46 +0300

    Change XF sample

--- samples/Xamarin.Forms-sample/TestNugetDeviceOrientationPlugin/TestNugetDeviceOrientationPlugin/MainPage.xaml.cs
+++ samples/Xamarin.Forms-sample/TestNugetDeviceOrientationPlugin/TestNugetDeviceOrientationPlugin/MainPage.xaml.cs
@@ -1,21 +1,35 @@
 ﻿using Xamarin.Forms;
+
 using Plugin.DeviceOrientation;
 
 namespace TestNugetDeviceOrientationPlugin
 {
     public partial class MainPage : ContentPage
     {
+        private bool _isLocked;
+
         public MainPage()
         {
             InitializeComponent();
 
             OrientationLabel.Text = CrossDeviceOrientation.Current.CurrentOrientation.ToString();
-            CrossDeviceOrientation.Current.OrientationChanged += Current_OrientationChanged;
-        }
 
-        private void Current_OrientationChanged(object sender, Plugin.DeviceOrientation.Abstractions.OrientationChangedEventArgs e)
-        {
-            OrientationLabel.Text = e.Orientation.ToString();
+            // sample of screen orientation lock
+            OrientationLabel.Clicked += (sender, args) =>
+            {
+                _isLocked = !_isLocked;
+
+                if (_isLocked)
+                    CrossDeviceOrientation.Current.LockOrientation(CrossDeviceOrientation.Current.CurrentOrientation);
+                else
+                    CrossDeviceOrientation.Current.UnlockOrientation();
+            };
+
+            // the sample of the event handler when screen orientation will change
+            CrossDeviceOrientation.Current.OrientationChanged += (sender, args) =>
+            {
+                OrientationLabel.Text = args.Orientation.ToString();
+            };
         }
     }
 }
