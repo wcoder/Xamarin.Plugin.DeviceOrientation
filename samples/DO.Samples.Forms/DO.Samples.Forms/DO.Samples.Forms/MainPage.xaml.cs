4efc18181315d021a2803f635664b9dcd25a44f3
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2018-02-05 21:23:12 +0300

    Improve sample.

--- samples/DO.Samples.Forms/DO.Samples.Forms/DO.Samples.Forms/MainPage.xaml.cs
+++ samples/DO.Samples.Forms/DO.Samples.Forms/DO.Samples.Forms/MainPage.xaml.cs
@@ -3,17 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Plugin.DeviceOrientation;
+using Plugin.DeviceOrientation.Abstractions;
 using Xamarin.Forms;
 
 namespace DO.Samples.Forms
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
+    public partial class MainPage : ContentPage
+    {
+        private bool _isLocked = false;
+
+        public MainPage()
+        {
+            InitializeComponent();
 
             var a = new DSImage(1, 2, 3, 4);
-		}
-	}
+
+            CrossDeviceOrientation.Current.OrientationChanged += CurrentOnOrientationChanged;
+        }
+
+        private void CurrentOnOrientationChanged(object sender, OrientationChangedEventArgs orientationChangedEventArgs)
+        {
+            btn.Text = orientationChangedEventArgs.Orientation.ToString();
+        }
+
+        public void Button_Click(object sender, EventArgs args)
+        {
+            if (_isLocked)
+                CrossDeviceOrientation.Current.LockOrientation(CrossDeviceOrientation.Current.CurrentOrientation);
+            else
+                CrossDeviceOrientation.Current.UnlockOrientation();
+
+            _isLocked = !_isLocked;
+        }
+    }
 }
