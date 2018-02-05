687a88674f8739f68a4728e42c3b043faa151ab3
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2018-02-05 18:59:11 +0300

    Add new sample project for Xamarin.Forms

---
+++ samples/DO.Samples.Forms/DO.Samples.Forms/DO.Samples.Forms.Android/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Plugin.CurrentActivity;
+using Android.Content.Res;
+using Plugin.DeviceOrientation;
+
+namespace DO.Samples.Forms.Droid
+{
+    [Activity(Label = "DO.Samples.Forms", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            // Step 1:
+            CrossCurrentActivity.Current.Activity = this;
+
+            // Step 2:
+            DeviceOrientationImplementation.Init();
+
+            // ...
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+
+        // Step 3:
+        public override void OnConfigurationChanged(Configuration newConfig)
+        {
+            base.OnConfigurationChanged(newConfig);
+
+            DeviceOrientationImplementation.NotifyOrientationChange(newConfig);
+        }
+    }
+}
+
---
+++ samples/DO.Samples.Forms/DO.Samples.Forms/DO.Samples.Forms/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DO.Samples.Forms
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            var a = new DSImage(1, 2, 3, 4);
+		}
+	}
+}
